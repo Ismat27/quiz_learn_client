a5c4708e1fe35b9d8526db6a790ebc794c86a4d3
unknown <adegbiteismail27@gmail.com>
2022-09-15 06:53:06 -0700

    connect to login api

--- src/features/auth/LoginForm.jsx
+++ src/features/auth/LoginForm.jsx
@@ -1,14 +1,55 @@
+import { useState } from "react"
+
 const LoginForm = () => {
+    const [username, setUsername ]= useState('')
+    const [password, setPassword] = useState('')
+
+    const formSubmit = (event) => {
+        event.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        };
+        fetch('http://127.0.0.1:8000/login/', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log(data)
+                    setPassword('')
+                    setUsername('')
+                }
+                else {
+                    console.log('error');
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
         <main>
-            <form>
+            <form onSubmit={formSubmit}>
                 <div className="username">
                     <label htmlFor="username">Username</label>
-                    <input id="username" name="username" type="text" />
+                    <input
+                        id="username"
+                        name="username" 
+                        type="text" 
+                        value={username}
+                        onChange={event => setUsername(event.target.value)}
+                    />
                 </div>
                 <div className="password">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
                 </div>
                 <button className="btn loginBtn">login</button>
             </form>
