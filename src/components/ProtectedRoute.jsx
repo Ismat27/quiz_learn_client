851c5bf3d42da05621d572b6bff4d8a5e6d79faa
unknown <adegbiteismail27@gmail.com>
2022-09-15 12:13:20 -0700

    private route

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom"
+
+const ProtectedRoute = ({user}) => {
+    if (!user) {
+        return <Navigate to={'/'} />
+    }
+
+    return (
+        <Outlet/>
+    )
+}
+
+export default ProtectedRoute
