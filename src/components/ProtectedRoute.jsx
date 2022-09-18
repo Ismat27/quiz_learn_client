ddac890ee9707955ccba172d422ad084ffa0bfd9
unknown <adegbiteismail27@gmail.com>
2022-09-18 12:22:09 -0700

    updates

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
-import { useContext, useState } from "react"
+import { useContext } from "react"
 import { Link, Navigate, Outlet } from "react-router-dom"
 import { Context } from "../app/AppContext"
 
 const ProtectedRoute = () => {
-    const [count, setCount] = useState(0)
     const {login, setLogin, setToken} = useContext(Context)
     if (!login) {
         return <Navigate to={'/'} replace />
@@ -19,11 +18,6 @@ const ProtectedRoute = () => {
                 <Link onClick={logout} to={'/'}>log out</Link>
             </nav>
             <Outlet/>
-            <div>
-                <button onClick={() => setCount(prev => prev - 1)}>-</button>
-                <span>{count}</span>
-                <button onClick={() => setCount(prev => prev + 1)}>+</button>
-            </div>
         </>
         
     )
