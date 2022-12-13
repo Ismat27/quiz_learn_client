a0434cdbbe4d2365eae384d892d255f306bb21ec
ismat27 <adegbiteismail27@gmail.com>
2022-12-12 22:36:09 -0800

    component for displaying user earnings

---
+++ src/features/dashboard/components/EarningsHistory.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const EarningsHistory = () => {
+  return (
+    <Wrapper>
+        <h2 className='capitalize'>earnings history</h2>
+        <div></div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+`
+
+export default EarningsHistory
