ace49bc3c0967c701e336e1743239ae12d57df2d
ismat27 <adegbiteismail27@gmail.com>
2022-12-14 11:38:14 -0800

    updates

--- src/features/dashboard/pages/LeaderBoard.jsx
+++ src/features/dashboard/pages/LeaderBoard.jsx
@@ -27,30 +27,77 @@ const LeaderBoard = () => {
 
   return (
     <Wrapper>
-      <h1>LeaderBoard</h1>
-      <div>
+      <div className="capitalize section-header">
+        <h1 className="section-title">Leaderboard</h1>
+        <p className="section-intro">view the leaderboard history</p>
+      </div>
+      {
+        leaderboardData.length > 0 ?
+      <div role={'table'} className="leaderboard">
+        <div className="board-item">
+          <p>&nbsp;</p>
+          <p>name</p>
+          <p className="score-value">total answered</p>
+          <p>reward</p>
+        </div>
         {
           leaderboardData.map((item, index) => {
             return (
-              <div className="board-item" key={index}>
+              <div role={'row'} className="board-item" key={index}>
+                <p>{index + 1}</p>
                 <p>{item.name}</p>
-                <p>{item.challenge_points}</p>
-                <p>{item.course_access_points}</p>
-                <p>{item.total_points}</p>
+                <p className="score-value">{item.score}</p>
+                <p className="reward">{item.total_points}pts</p>
               </div>
             )
           })
         }
-      </div>
+      </div> :
+      <p>no data</p>
+      }
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
+padding-block: 1rem 2rem;
+.section-intro {
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.49);
+  font-size: 18px;
+  margin-top: .5rem;
+}
+.leaderboard {
+  margin-top: 3rem;
+}
 .board-item {
   display: grid;
-  grid-template-columns: 7fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 4fr 4fr 1fr;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.62);
+  padding-block: 1.2rem;
   text-transform: capitalize;
+  p {
+    font-weight: bold;
+  }
+}
+.board-item:last-child {
+  border: none;
+}
+.reward {
+  text-transform: none;
+}
+.score-value {
+  text-align: center;
+}
+
+@media (min-width: 576px) {
+  .board-item {
+    grid-template-columns: 1fr 5fr 3fr 1fr;
+}
+@media (min-width: 992px) {
+  padding-block: 2rem;
+
+}
 }
 `
 
