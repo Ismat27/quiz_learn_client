0ee8d5358384f519af4fa149feebc947783d6bd8
ismat27 <adegbiteismail27@gmail.com>
2022-12-14 11:03:31 -0800

    temporary file for quiz categories

---
+++ src/features/dashboard/data/quizData.js
@@ -0,0 +1,36 @@
+const quizCat = [
+    {
+        id: 1,
+        name: 'advance in cryptocurrency'
+    },
+    {
+        id: 2,
+        name: 'UI/UX Design'
+    },
+    {
+        id: 3,
+        name: ' front-end development'
+    },
+    {
+        id: 4,
+        name: 'back-end development'
+    },
+    {
+        id: 5,
+        name: 'page management'
+    },
+    {
+        id: 6,
+        name: 'true africa history'
+    },
+    {
+        id: 7,
+        name: 'skin care productions'
+    },
+    {
+        id: 8,
+        name: 'africa healing and herbs methodology '
+    },
+]
+
+export {quizCat}
