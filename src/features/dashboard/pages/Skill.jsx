b2856d109a0d997cfea748092a54e5c2fee225ae
ismat27 <adegbiteismail27@gmail.com>
2022-12-18 12:56:48 -0800

    updates

--- src/features/dashboard/pages/Skill.jsx
+++ src/features/dashboard/pages/Skill.jsx
@@ -1,7 +1,32 @@
+import Courses from "../components/Courses"
+import { courses as tempdata } from "../data/course"
+import styled from "styled-components"
+
+
 const Skill = () => {
   return (
-    <div>Skill</div>
+    <Wrapper>
+      <Courses 
+        data={tempdata[0]}
+      />
+      <Courses 
+        data={tempdata[1]}
+      />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+padding-block: 1rem;
+@media (min-width: 576px) {
+  
+}
+@media (min-width: 768px) {
+
+}
+@media (min-width: 992px) {
+  padding-block: 2rem;
+}
+`
 
 export default Skill
