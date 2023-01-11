d609071059f59e7a3038aaa06c48b58012f4046f
ismat27 <adegbiteismail27@gmail.com>
2023-01-11 03:55:25 -0800

    updating app to access dashboard ceontex

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppContext from './app/AppContext';
+import DashboardContext from './context/DashboardContext';
 
 const tkn = localStorage.getItem('token')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
         <AppContext stored_token={tkn}>
-            <App />
+            <DashboardContext>
+                <App />
+            </DashboardContext>
         </AppContext>
 );
