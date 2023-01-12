376884ed671578d96566dc241965827197bc5de8
ismat27 <adegbiteismail27@gmail.com>
2023-01-12 02:55:18 -0800

    catch loading and error states during data fetching

--- src/features/dashboard/DashBoard.jsx
+++ src/features/dashboard/DashBoard.jsx
@@ -4,8 +4,12 @@ import styled from "styled-components"
 import Hamburger from "../../components/Hamburger"
 import SidebarNav from "./components/SidebarNav"
 import Logo from "../../components/Logo"
+import { useDashboardContext } from "../../context/DashboardContext"
+import CircularLoader from "../../components/CircularLoader"
 
 const Dashboard = () => {
+
+    const { error, loading } = useDashboardContext()
 
     const [sideOpen, setSideOpen] = useState(false)
 
@@ -15,6 +19,27 @@ const Dashboard = () => {
 
     const closeSidebar = () => {
         setSideOpen(false)
+    }
+
+    if (error) {
+        return (
+            <Wrapper>
+                <div className="loading-div">
+                    <h1>an error occured, pls reload or logout and login again</h1>
+                </div>
+            </Wrapper>
+        )
+    }
+    
+    if (loading) {
+        return (
+            <Wrapper>
+                <div className="loading-div">
+                    <h1>loading...</h1>
+                    <CircularLoader />
+                </div>
+            </Wrapper>
+        )
     }
     
     return (
@@ -47,6 +72,12 @@ const Dashboard = () => {
 }
 
 const Wrapper = styled.div`
+.header {
+    align-items: flex-start;
+}
+.loading-div {
+    text-align: center;
+}
 @media (min-width: 992px) {
 .aa {
     margin-left: 250px;
